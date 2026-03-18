a1b9b88ef29d085c026f299d0cd7860bddd4e62d
yogiraj2224 <yogiraj.haladkar40@gmail.com>
2026-03-18 15:20:15 +0530

    removed the latitude and longitude

--- frontend/vehicleTelemetry/src/components/Dynamic/DyanamicData.jsx
+++ frontend/vehicleTelemetry/src/components/Dynamic/DyanamicData.jsx
@@ -9,42 +9,27 @@ export default function DynamicData() {
 
     return (
         <div className=" p-8">
-            <h2 className="font-bold mb-2">Dynamic Data</h2>
-
-
-            
             <p><strong>Altitude:</strong> {dynamicData.altitude}</p>
-
-
             <p><strong>Speed:</strong> {dynamicData.speed} km/h</p>
             <p><strong>Status:</strong> {dynamicData.status}</p>
             <p><strong>Gear:</strong> {dynamicData.gear}</p>
             <p><strong>Avg Speed:</strong> {dynamicData.avgSpeed}</p>
-
             <p><strong>RPM:</strong> {dynamicData.rpm}</p>
             <p><strong>Ignition:</strong> {dynamicData.ignition}</p>
             <p><strong>GPS Fix:</strong> {dynamicData.gpsFix}</p>
             <p><strong>GSM Signal:</strong> {dynamicData.gsmSignal}</p>
             <p><strong>Satellites:</strong> {dynamicData.satellites}</p>
-
             <p><strong>Fuel Level:</strong> {dynamicData.fuelLevel}%</p>
             <p><strong>Fuel Consumption:</strong> {dynamicData.fuelConsumption}</p>
-
             <p><strong>Battery Voltage:</strong> {dynamicData.batteryVoltage}</p>
             <p><strong>Battery Current:</strong> {dynamicData.batteryCurrent}</p>
-
             <p><strong>Engine Temperature:</strong> {dynamicData.engineTemperature}</p>
-
             <p><strong>Accel X:</strong> {dynamicData.accelX}</p>
             <p><strong>Accel Y:</strong> {dynamicData.accelY}</p>
             <p><strong>Accel Z:</strong> {dynamicData.accelZ}</p>
-
             <p><strong>Trip Distance:</strong> {dynamicData.tripDistance}</p>
             <p><strong>Odometer:</strong> {dynamicData.odometer}</p>
-
             <p><strong>Tyre Pressure:</strong> {dynamicData.tyrePressure?.join(", ")}</p>
-
-
             <p><strong>Timestamp:</strong> {dynamicData.timestamp}</p>
         </div>
     );
